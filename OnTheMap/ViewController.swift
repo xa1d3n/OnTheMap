d941743c5297f22d9cf282d1d2c7d1660011f814
Aldin Fajic <afajic@hotmail.com>
2015-06-29 19:44:10 -0400

    - login view setup

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("test")
+        println("testing")
     }
 
     override func didReceiveMemoryWarning() {
