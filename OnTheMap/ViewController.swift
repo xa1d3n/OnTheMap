b149a121f2a57ef610274153f9ca1ea717d63b37
Aldin Fajic <afajic@hotmail.com>
2015-07-01 23:18:37 -0400

    - added map view

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -41,29 +41,6 @@ class ViewController: UIViewController {
     }
     
     func loginUser() {
-       /* let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
-        request.HTTPMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.HTTPBody = "{\"udacity\": {\"username\": \"\(self.emailInput.text)\", \"password\": \"\(self.passwordInput.text)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { data, response, error in
-            if error != nil { // Handle error…
-                return
-            }
-            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
-           // println(NSString(data: newData, encoding: NSUTF8StringEncoding))
-        
-            //println(NSString(data: data, encoding: NSUTF8StringEncoding))
-            let jsonString = NSString(data: newData, encoding: NSUTF8StringEncoding)
-            let dataStuff = NSData(data: newData)
-            
-            var parseError: NSError? = nil
-            let parseStuff = NSJSONSerialization.JSONObjectWithData(dataStuff, options: NSJSONReadingOptions.allZeros, error: &parseError) as! NSDictionary
-            println(parseStuff["error"])
-            
-        }
-        task.resume() */
         
         UdacityCleint.sharedInstance().loginToUdacity(emailInput.text, password: passwordInput.text) { (result, error) -> Void in
             if error != nil {
